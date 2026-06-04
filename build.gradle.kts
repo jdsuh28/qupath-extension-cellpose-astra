d924b81ebd1652a357ec26710754f0f5eab3a953
astra-ci <astra-ci@users.noreply.github.com>
2026-06-04 04:06:57 +0000

    ASTRA: Release v0.1.125 metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 qupathExtension {
     name = "qupath-extension-cellpose-astra"
     group = "io.github.jdsuh28"
-    version = "0.1.124"
+    version = "0.1.125"
     description = "ASTRA QuPath extension for Cellpose-backed tissue analysis"
     automaticModule = "qupath.ext.astra.cellpose"
 }
